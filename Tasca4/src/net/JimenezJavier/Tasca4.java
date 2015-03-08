87e9381a849cbeb26bef7ae94455723917b638e3
Surrui <Surrui@HAL9000>
2015-03-08 02:15:06 +0100

    First Commit

---
+++ Tasca4/src/net/JimenezJavier/Tasca4.java
@@ -0,0 +1,12 @@
+package net.JimenezJavier;
+
+public class Tasca4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Mon!");
+		
+	}
+
+}
---
+++ Tasca4/src/net/JimenezJavier/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Surrui
+ *
+ */
+package net.JimenezJavier;
